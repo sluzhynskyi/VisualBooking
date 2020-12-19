98c991958c6ace87e244128f0a6d2839d30c1c89
Danylo Sluzhynskyi <sluzhynskyi@gmail.com>
2020-12-19 17:30:36 +0200

    with deletion reservation

--- VisualBooking/ViewControllers/ReservationListController.swift
+++ VisualBooking/ViewControllers/ReservationListController.swift
@@ -66,6 +66,18 @@ class ReservationListController: UIViewController, UITableViewDelegate, UITableV
         return
     }
 
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath)
+        -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: nil) { (_, _, completionHandler) in
+
+            FIRFirestoreService.shared.delete(self.userReservations[indexPath.row], in: .reservations)
+            completionHandler(true)
+        }
+        deleteAction.image = UIImage(systemName: "trash")
+        deleteAction.backgroundColor = .systemRed
+        let configuration = UISwipeActionsConfiguration(actions: [deleteAction])
+        return configuration
+    }
 }
 
 extension ReservationListController {
